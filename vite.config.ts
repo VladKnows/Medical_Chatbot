b627a9f3a6f103ecc195ae675d61068fe4b0b25e
Catalin.BUTACU <ic.butacu@lectra.com>
2025-10-22 14:27:52 +0300

    uwu

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
+  base: '/Medical_Chatbot/',
   plugins: [react()],
   resolve: {
     alias: {
