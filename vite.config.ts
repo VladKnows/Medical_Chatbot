afb6105b771fa88da431da2dded586f0aaf18ead
Catalin.BUTACU <ic.butacu@lectra.com>
2025-10-22 09:56:46 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
+      '@/lib': path.resolve(__dirname, 'src/lib'),
+      '@/components': path.resolve(__dirname, 'src/components'),
+      '@/pages': path.resolve(__dirname, 'src/pages'),
+      '@/hooks': path.resolve(__dirname, 'src/hooks'),
+      '@/assets': path.resolve(__dirname, 'src/assets'),
     },
   },
   server: {
